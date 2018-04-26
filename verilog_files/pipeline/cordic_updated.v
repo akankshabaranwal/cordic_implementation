7bf051e5252e773d33954d2af295785fc3b3fbf9
akankshabaranwal <akankshabar@gmail.com>
2018-04-26 19:29:44 +0530

    debug

--- verilog_files/pipeline/cordic_updated.v
+++ verilog_files/pipeline/cordic_updated.v
@@ -28,6 +28,9 @@ output reg [31:0] y
 );
 
 	reg [31:0] tanarray [0:15];
+	wire [31:0] x [0:15];
+	wire [31:0] y [0:15];
+	wire [31:0] z [0:15];
 	parameter i0=0;
 	parameter i1=1;
 	parameter i2=2;
@@ -64,22 +67,22 @@ output reg [31:0] y
 		tanarray[15]<=32'd0;
 	end
 
-	  shift_accumulate shift_accumulate0(.x(x0),.y(y0),.z(z0),.clk(clk),.i(i0),.x_out(x1),.y_out(y1),.z_out(z1));
-	  shift_accumulate shift_accumulate1(.x(x1),.y(y1),.z(z1),.clk(clk),.i(i1),.x_out(x2),.y_out(y2),.z_out(z2));
-	  shift_accumulate shift_accumulate2(.x(x2),.y(y2),.z(z2),.clk(clk),.i(i2),.x_out(x3),.y_out(y3),.z_out(z3));
-	  shift_accumulate shift_accumulate3(.x(x3),.y(y3),.z(z3),.clk(clk),.i(i3),.x_out(x4),.y_out(y4),.z_out(z4));
-	  shift_accumulate shift_accumulate4(.x(x4),.y(y4),.z(z4),.clk(clk),.i(i4),.x_out(x5),.y_out(y5),.z_out(z5));
-	  shift_accumulate shift_accumulate5(.x(x5),.y(y5),.z(z5),.clk(clk),.i(i5),.x_out(x6),.y_out(y6),.z_out(z6));
-	  shift_accumulate shift_accumulate6(.x(x6),.y(y6),.z(z6),.clk(clk),.i(i6),.x_out(x7),.y_out(y7),.z_out(z7));
-	  shift_accumulate shift_accumulate7(.x(x7),.y(y7),.z(z7),.clk(clk),.i(i7),.x_out(x8),.y_out(y8),.z_out(z8));
-	  shift_accumulate shift_accumulate8(.x(x8),.y(y8),.z(z8),.clk(clk),.i(i8),.x_out(x9),.y_out(y9),.z_out(z9));
-	  shift_accumulate shift_accumulate9(.x(x9),.y(y9),.z(z9),.clk(clk),.i(i9),.x_out(x10),.y_out(y10),.z_out(z10));
-	  shift_accumulate shift_accumulate10(.x(x10),.y(y10),.z(z10),.clk(clk),.i(i10),.x_out(x11),.y_out(y11),.z_out(z11));
-	  shift_accumulate shift_accumulate11(.x(x11),.y(y11),.z(z11),.clk(clk),.i(i11),.x_out(x12),.y_out(y12),.z_out(z12));
-	  shift_accumulate shift_accumulate12(.x(x12),.y(y12),.z(z12),.clk(clk),.i(i12),.x_out(x13),.y_out(y13),.z_out(z13));
-	  shift_accumulate shift_accumulate13(.x(x13),.y(y13),.z(z13),.clk(clk),.i(i13),.x_out(x14),.y_out(y14),.z_out(z14));
-	  shift_accumulate shift_accumulate14(.x(x14),.y(y14),.z(z14),.clk(clk),.i(i14),.x_out(x15),.y_out(y15),.z_out(z15));
-	  shift_accumulate shift_accumulate15(.x(x15),.y(y15),.z(z15),.clk(clk),.i(i15),.x_out(x16),.y_out(y16),.z_out(z16));
+	shift_accumulate shift_accumulate0(.x(x0),.y(y0),.z(z0),.clk(clk),.i(i0),.x_out(x[1]),.y_out(y[1]),.z_out(z[1]),.tan(tanarray[0]));
+	shift_accumulate shift_accumulate1(.x(x[1]),.y(y[1]),.z(z[1]),.clk(clk),.i(i1),.x_out(x[2]),.y_out(y[2]),.z_out(z[2]),.tan(tanarray[1]));
+	shift_accumulate shift_accumulate2(.x(x[2]),.y(y[2]),.z(z[2]),.clk(clk),.i(i2),.x_out(x[3]),.y_out(y[3]),.z_out(z[3]),.tan(tanarray[2]));
+	shift_accumulate shift_accumulate3(.x(x[3]),.y(y[3]),.z(z[3]),.clk(clk),.i(i3),.x_out(x[4]),.y_out(y[4]),.z_out(z[4]),.tan(tanarray[3]));
+	shift_accumulate shift_accumulate4(.x(x[4]),.y(y[4]),.z(z[4]),.clk(clk),.i(i4),.x_out(x[5]),.y_out(y[5]),.z_out(z[5]),.tan(tanarray[4]));
+	shift_accumulate shift_accumulate5(.x(x[5]),.y(y[5]),.z(z[5]),.clk(clk),.i(i5),.x_out(x[6]),.y_out(y[6]),.z_out(z[6]),.tan(tanarray[5]));
+	shift_accumulate shift_accumulate6(.x(x[6]),.y(y[6]),.z(z[6]),.clk(clk),.i(i6),.x_out(x[7]),.y_out(y[7]),.z_out(z[7]),.tan(tanarray[6]));
+	shift_accumulate shift_accumulate7(.x(x[7]),.y(y[7]),.z(z[7]),.clk(clk),.i(i7),.x_out(x[8]),.y_out(y[8]),.z_out(z[8]),.tan(tanarray[7]));
+	shift_accumulate shift_accumulate8(.x(x[8]),.y(y[8]),.z(z[8]),.clk(clk),.i(i8),.x_out(x[9]),.y_out(y[9]),.z_out(z[9]),.tan(tanarray[8]));
+	shift_accumulate shift_accumulate9(.x(x[9]),.y(y[9]),.z(z[9]),.clk(clk),.i(i9),.x_out(x[10]),.y_out(y[10]),.z_out(z[10]),.tan(tanarray[9]));
+	shift_accumulate shift_accumulate10(.x(x[10]),.y(y[10]),.z(z[10]),.clk(clk),.i(i10),.x_out(x[11]),.y_out(y[11]),.z_out(z[11]),.tan(tanarray[10]));
+	shift_accumulate shift_accumulate11(.x(x[11]),.y(y[11]),.z(z[11]),.clk(clk),.i(i11),.x_out(x[12]),.y_out(y[12]),.z_out(z[12]),.tan(tanarray[11]));
+	shift_accumulate shift_accumulate12(.x(x[12]),.y(y[12]),.z(z[12]),.clk(clk),.i(i12),.x_out(x[13]),.y_out(y[13]),.z_out(z[13]),.tan(tanarray[12]));
+	shift_accumulate shift_accumulate13(.x(x[13]),.y(y[13]),.z(z[13]),.clk(clk),.i(i13),.x_out(x[14]),.y_out(y[14]),.z_out(z[14]),.tan(tanarray[13]));
+	shift_accumulate shift_accumulate14(.x(x[14]),.y(y[14]),.z(z[14]),.clk(clk),.i(i14),.x_out(x[15]),.y_out(y[15]),.z_out(z[15]),.tan(tanarray[14]));
+	shift_accumulate shift_accumulate15(.x(x[15]),.y(y[15]),.z(z[15]),.clk(clk),.i(i15),.x_out(x[16]),.y_out(y[16]),.z_out(z[16]),.tan(tanarray[15]));
 
 	always@(posedge clk)
 	  begin
